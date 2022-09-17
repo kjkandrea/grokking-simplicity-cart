5de9f58ef9fac3e3db9d7507acd1931f4eab52f2
kjkandrea <kjkandrea@gmail.com>
2022-09-17 12:41:48 +0900

    chore: 불필요 코드 제거

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,7 @@ import setup from './cart'
 import {getCartData} from "./cart/data/cart";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <div id="cart"></div>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
+  <div id="cart"></div>
 `
 
 const cart = setup(document.querySelector<HTMLDivElement>('#cart')!, getCartData());
