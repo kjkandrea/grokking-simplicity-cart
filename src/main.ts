86715fd44a97f5611c555d782a079fbcda2bb286
kjkandrea <kjkandrea@gmail.com>
2022-09-20 16:18:06 +0900

    improve: 각 렌더러 별 섹션 분리
    리팩터링 필요

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,34 @@ import {getCartData} from './cart/data/cart';
 import {CartRenderer, CartTotalRenderer} from './cart';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div id="cart"></div>
+  <h2>장바구니</h2>
+  <section id="cart"></section>
+  <h2>즉시배송가능 상품</h2>
+  <section id="immediate-deliverable-cart"></section>
+  <h2>총 결제 금액</h2>
+  <section id="cart-total"></section>
 `;
 
 const cartData = getCartData();
-const cartRootElement = document.querySelector<HTMLDivElement>('#cart')!;
+
+const cartRootElement = document.getElementById('cart')!;
+const immediateDeliverableCartRootElement = document.getElementById(
+  'immediate-deliverable-cart'
+)!;
+const cartTotalRootElement = document.getElementById('cart-total')!;
 
 const cart = setup(cartData);
 
 const cartRenderer = new CartRenderer(cartRootElement);
-const cartTotalRenderer = new CartTotalRenderer(cartRootElement);
+const immediateDeliverableCartRenderer = new CartRenderer(
+  immediateDeliverableCartRootElement
+);
+const cartTotalRenderer = new CartTotalRenderer(cartTotalRootElement);
 
 cartRenderer.render(cartData);
 cart.subscribe(cartData => {
   cartRenderer.render(cartData);
+  immediateDeliverableCartRenderer.render(cartData);
   cartTotalRenderer.render(cart.totalPrice);
 });
 
