840de1f5320791e43db051e27a6070d582d175bd
kjkandrea <kjkandrea@gmail.com>
2022-09-25 13:10:50 +0900

    test: refactor

--- src/utils/object.spec.ts
+++ src/utils/object.spec.ts
@@ -2,31 +2,23 @@ import object from './object';
 
 describe('utils/object', () => {
   describe('object.update 는', () => {
+    const birdCount = {
+      hawk: 5,
+    };
     const increment = (count: number) => count + 1;
 
     it('modify 콜백 인자를 통해 값을 업데이트 한다.', () => {
-      const birdCount = {
-        hawk: 5,
-      };
-
       const newBirdCount = object.update(birdCount, 'hawk', increment);
       expect(newBirdCount.hawk).toBe(6);
     });
 
     it('key 값이 존재하지 않는다면 에러를 반환한다.', () => {
-      const birdCount = {
-        hawk: 5,
-      };
-
       expect(() =>
         object.update(birdCount, 'dog' as any, increment)
       ).toThrowError();
     });
 
     it('인자로 전달받은 원본 객체의 값을 변경하지 않는다.', () => {
-      const birdCount = {
-        hawk: 5,
-      };
       const newBirdCount = object.update(birdCount, 'hawk', increment);
 
       expect(birdCount.hawk).toBe(5);
