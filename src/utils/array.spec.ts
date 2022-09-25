35239ba958053811a5c7a8f34d1a252f107058d5
kjkandrea <kjkandrea@gmail.com>
2022-09-25 14:37:50 +0900

    test: array.dropFirst 스팩 추가

--- src/utils/array.spec.ts
+++ src/utils/array.spec.ts
@@ -1,18 +1,18 @@
 import array from './array';
 
 describe('utils/array', () => {
-  describe('array.pluck 는', () => {
-    const doggies = [
-      {
-        name: 'corgi',
-        age: 1,
-      },
-      {
-        name: 'retriever',
-        age: 2,
-      },
-    ];
+  const doggies = [
+    {
+      name: 'corgi',
+      age: 1,
+    },
+    {
+      name: 'retriever',
+      age: 2,
+    },
+  ];
 
+  describe('array.pluck 는', () => {
     it('fieldName 을 통해 fieldName 의 value 를 통해 배열을 만든다.', () => {
       const doggyNames = array.pluck(doggies, 'name');
       expect(doggyNames.join()).toBe('corgi,retriever');
@@ -23,6 +23,22 @@ describe('utils/array', () => {
       expect(doggyFirstNames.every(firstName => firstName === undefined)).toBe(
         true
       );
+    });
+  });
+
+  describe('array.dropFirst 는', () => {
+    it('앞에서 부터 첫번째 아이템이 지워진 배열을 리턴한다.', () => {
+      const newDoggies = array.dropFirst(doggies);
+      expect(newDoggies.length).toBe(doggies.length - 1);
+      const [firstDoggy] = newDoggies;
+      expect(firstDoggy.name).toBe('retriever');
+    });
+
+    it('인자로 전달받은 원본 배열의 값을 변경하지 않는다.', () => {
+      const newDoggies = array.dropFirst(doggies);
+      const [corgi] = doggies;
+      expect(corgi.name).toBe('corgi');
+      expect(doggies !== newDoggies).toBe(true);
     });
   });
 });
