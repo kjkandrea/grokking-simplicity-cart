e45b87a47959290abd2507e45bc49c8ec183a79d
kjkandrea <kjkandrea@gmail.com>
2022-09-25 14:30:33 +0900

    test: array.pluck 스팩 테스트 추가 2

--- src/utils/array.spec.ts
+++ src/utils/array.spec.ts
@@ -1,4 +1,5 @@
 import array from './array';
+import object from './object';
 
 describe('utils/array', () => {
   describe('array.pluck 는', () => {
@@ -16,6 +17,13 @@ describe('utils/array', () => {
     it('fieldName 을 통해 fieldName 의 value 를 통해 배열을 만든다.', () => {
       const doggyNames = array.pluck(doggies, 'name');
       expect(doggyNames.join()).toBe('corgi,retriever');
+    });
+
+    it('key 값이 존재하지 않는다면 undefined 가 담긴 배열을 반환한다.', () => {
+      const doggyFirstNames = array.pluck(doggies, 'firstName' as any);
+      expect(doggyFirstNames.every(firstName => firstName === undefined)).toBe(
+        true
+      );
     });
   });
 });
