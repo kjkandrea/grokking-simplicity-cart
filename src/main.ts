67a1f0ad3fde10683d2021fc7aef71e9c4cf9b19
kjkandrea <kjkandrea@gmail.com>
2022-09-25 12:43:42 +0900

    chore: setCartItemOptionBy 사용 케이스 추가

--- src/main.ts
+++ src/main.ts
@@ -75,4 +75,5 @@ const cartController = new CartController(
 
 cartController.cart.setCartItemFieldBy('shoes', 'price', 500);
 cartController.cart.setCartItemFieldBy('tShort', 'price', 800);
+cartController.cart.setCartItemOptionBy('hood', 'color', 'deep dark');
 cartController.cart.setCartItemOptionBy('tShort', 'size', 5);
