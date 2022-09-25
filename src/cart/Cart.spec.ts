1789079efc70eaa8de019e43806f3c797c8698b4
kjkandrea <kjkandrea@gmail.com>
2022-09-25 10:52:58 +0900

    feat: Cart immediateDeliverable 테스트 추가

--- src/cart/Cart.spec.ts
+++ src/cart/Cart.spec.ts
@@ -1,4 +1,5 @@
 import {Cart} from './Cart';
+import {Cart as CartData} from './data/cart';
 
 describe('Cart', () => {
   describe('totalPrice 는', () => {
@@ -22,5 +23,32 @@ describe('Cart', () => {
   });
 
   // TODO: immediateDeliverable 테스트 작성
-  describe('immediateDeliverable 는', () => {});
+  describe('immediateDeliverable 는', () => {
+    it('즉시배송 가능 : true 인 상품들만 반환한다.', () => {
+      const cartData: CartData = {
+        shoes: {
+          price: 1000,
+          quantity: 1,
+          shipping: 'seoul',
+          immediateDeliverable: true,
+        },
+        tShort: {
+          price: 1500,
+          quantity: 2,
+          shipping: 'my company',
+        },
+      };
+
+      const {immediateDeliverableCart} = new Cart(cartData);
+      const immediateDeliverableCartList = Object.values(
+        immediateDeliverableCart
+      );
+
+      expect(
+        immediateDeliverableCartList.every(
+          cartItem => cartItem.immediateDeliverable
+        )
+      ).toBe(true);
+    });
+  });
 });
